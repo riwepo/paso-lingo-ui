81f189d301fbbfb25c687c38274fc2e0e05aebcc
riwepo <riwepo.coding@gmail.com>
2026-03-05 09:04:00 +1100

    remove plugin from babel, not needed

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,16 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',
       [
         'module-resolver',
         {
-          root: ['./'],
           alias: {
             '@': './',
           },
         },
       ],
-      // ... your other plugins
     ],
   };
 };
